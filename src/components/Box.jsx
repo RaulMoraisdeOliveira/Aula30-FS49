cd3685dcc322317b50d7461fe550fc1343884d37
Raul Morais <raulmorais2010@gmail.com>
2025-11-27 21:53:49 -0300

    Calculator Project

---
+++ src/components/Box.jsx
@@ -0,0 +1,18 @@
+
+const Box = ({ children, display, columns, gap, padding }) => {
+
+  const style = `
+    ${display}
+    ${columns}
+    ${gap}
+    ${padding}
+  `;
+
+  return (
+    <>
+      <div className={style}>{children}</div>
+    </>
+  );
+}
+
+export default Box;
